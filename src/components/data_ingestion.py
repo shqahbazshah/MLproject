d6cf2ec5fe184cc89b0c8b231f26ca2febb399ed
Shahbaz Shah <shahbazshahkazmi77@gmail.com>
2025-11-28 20:04:44 +0500

    model train step complete

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,6 +6,7 @@ from sklearn.model_selection import train_test_split
 import logging
 from src.exception import CustomException
 from src.components.data_transformation import DataTransformation, DataTransformationConfig
+from src.components.model_trainer import ModelTrainer,  ModelTrainerConfig
 
 @dataclass
 class DataIngestionConfig:
@@ -45,8 +46,16 @@ class DataIngestion:
             raise CustomException(e, sys)
 if __name__ == '__main__':
     obj = DataIngestion(DataIngestionConfig())
-    train_path,test_path = obj.initiate_data_ingestion()
+    train_path, test_path = obj.initiate_data_ingestion()
 
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_path, test_path)
+    train_array, test_array, _ = data_transformation.initiate_data_transformation(train_path, test_path)
+
+    model_trainer = ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_array, test_array))
+
+
+
+
+
 
