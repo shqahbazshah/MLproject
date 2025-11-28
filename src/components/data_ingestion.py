6bb3c9338c66073b29a3a618c0a0c2626c4efd10
Shahbaz Shah <shahbazshahkazmi77@gmail.com>
2025-11-28 10:15:28 +0500

    data preprssing and ingestion comlete

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,52 @@
+import os
+import sys
+from dataclasses import dataclass
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import logging
+from src.exception import CustomException
+from src.components.data_transformation import DataTransformation, DataTransformationConfig
+
+@dataclass
+class DataIngestionConfig:
+    train_data_path: str = os.path.join('artifact', 'train.csv')
+    test_data_path: str = os.path.join('artifact', 'test.csv')
+    raw_data_path: str = os.path.join('artifact', 'data.csv')
+
+class DataIngestion:
+    def __init__(self, ingestion_config: DataIngestionConfig):
+        self.ingestion_config = ingestion_config
+
+    def initiate_data_ingestion(self):
+        logging.info('Entered the data ingestion method or component')
+        try:
+            df = pd.read_csv('notebook/data/stud.csv')
+            logging.info('Read the dataset as a dataframe')
+
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
+
+            df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
+            logging.info('Saved raw data to artifact folder')
+
+            logging.info('Initiating train test split')
+            train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
+
+            train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
+
+            logging.info('Data ingestion is completed')
+
+            return (
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+
+        except Exception as e:
+            raise CustomException(e, sys)
+if __name__ == '__main__':
+    obj = DataIngestion(DataIngestionConfig())
+    train_path,test_path = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_path, test_path)
+
--- src/exception.py
+++ src/exception.py
@@ -1,9 +1,11 @@
+
 def error_message_detail(error, error_detail):
     _, _, exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
     line_number = exc_tb.tb_lineno
     return "Error occurred in Python script name [{0}] line number [{1}] error message [{2}]".format(
         file_name, line_number, str(error)
+
     )
 
 
